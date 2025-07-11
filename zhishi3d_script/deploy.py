5abddfbda055d459ffb923a874298674f110cf8d
qq65108603 <651086039@qq.com>
2025-07-12 05:01:56 +0800

    自动提交：合并前提交本地更改

--- zhishi3d_script/deploy.py
+++
@@ -1,6 +0,0 @@
-from main import start_comfyui_app
-from zhishi3d.utils.root import init_config
-
-if __name__ == '__main__':
-    init_config('prod')
-    start_comfyui_app()
--- zhishi3d_script/install.sh
+++
@@ -1,2 +0,0 @@
-
-git clone https://codeup.aliyun.com/60d25897a3a2bac55d31158a/comfyui/zhishi3d.git
