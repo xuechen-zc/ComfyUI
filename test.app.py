caf58bb652fc5ebe1015acf4975289f099688462
qq65108603 <651086039@qq.com>
2025-07-14 17:25:18 +0800

    自动提交：合并前提交本地更改

--- test.app.py
+++
@@ -1,9 +0,0 @@
-import socket
-import sys
-import time
-
-from zhishi3d.app import redis_listener, ensure_single_instance
-
-if __name__ == '__main__':
-    ensure_single_instance()
-    redis_listener()
