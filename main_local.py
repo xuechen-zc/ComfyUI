4449a04790b0d186723a79518699a78042fa9dd8
qq65108603 <651086039@qq.com>
2025-08-26 12:54:45 +0800

    自动提交：合并前提交本地更改

---
+++ main_local.py
@@ -0,0 +1,9 @@
+from zhishi3d.utils.root import init_config
+init_config("local")
+from main import start_comfyui_app
+from zhishi3d.script.utils import start_checker
+
+
+if __name__ == '__main__':
+    start_checker()
+    start_comfyui_app()
