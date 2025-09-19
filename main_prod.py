023bb71c1102367d37760c28f8cc89e10bf5b386
qq651086039 <6510860399@qq.com>
2025-09-20 02:42:17 +0800

    1

--- main_prod.py
+++ main_prod.py
@@ -1,8 +1,6 @@
 from zhishi3d.root import init_config
 init_config("prod")
 from main import start_comfyui_app
-from zhishi3d.script.utils import start_checker
-
 
 if __name__ == '__main__':
     start_comfyui_app()
